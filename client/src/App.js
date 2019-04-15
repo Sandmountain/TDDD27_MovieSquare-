5d02c0091f373f60d77dd2739ecc984a8af10559
Sandmountain <sandtain@gmail.com>
2019-04-15 12:27:45 +0200

    Added temporary movie cards with titles

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container } from "reactstrap";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faStroopwafel } from "@fortawesome/free-solid-svg-icons";
+
+import AppNavbar from "./components/AppNavbar";
+import MainBody from "./components/MainBody";
+import Collage from "./components/Collage";
+
+library.add(faStroopwafel);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <AppNavbar />
+        <Container>
+          <Collage />
+          <MainBody />
+        </Container>
       </div>
     );
   }

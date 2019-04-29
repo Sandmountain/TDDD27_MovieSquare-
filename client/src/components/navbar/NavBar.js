3d3790220fcec26e666a773f8d1cf4e9c93a69eb
Sandmountain <sandtain@gmail.com>
2019-04-29 09:40:58 +0200

    removed old files

--- client/src/components/navbar/NavBar.js
+++ client/src/components/navbar/NavBar.js
@@ -1,17 +1,22 @@
 import React from "react";
 import AppBar from "material-ui/AppBar";
+import darkBaseTheme from "material-ui/styles/baseThemes/darkBaseTheme";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import getMuiTheme from "material-ui/styles/getMuiTheme";
 import Button from "material-ui/FlatButton/FlatButton";
 import { Link } from "react-router-dom";
 
 const NavBar = () => (
-  <AppBar title="MoveSquare">
-    <Button>
-      <Link to="/watchlist">WatchList</Link>
-    </Button>
-    <Button>
-      <Link to="/">Home</Link>
-    </Button>
-  </AppBar>
+  <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+    <AppBar title="MoveSquare">
+      <Button>
+        <Link to="/watchlist">WatchList</Link>
+      </Button>
+      <Button>
+        <Link to="/">Home</Link>
+      </Button>
+    </AppBar>
+  </MuiThemeProvider>
 );
 
 function goWatchList() {}
